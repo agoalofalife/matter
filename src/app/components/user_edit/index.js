837b1fbe2dad3c0cc6323ea09bef8fbf6903d89f
agoalofalife <agoalofalife@gmail.com>
2018-10-15 17:53:08 +0300

    try state expression in ui user edit

--- src/app/components/user_edit/index.js
+++ src/app/components/user_edit/index.js
@@ -10,7 +10,7 @@ let trueEmail = Value.query('data.email').as(email => validation.isEmail(email))
 let truePhone = Value.query('data.phone').as(phone => phone ? phone.replace(/\)/, '').replace(/\(/, '').replace(/\-+/, '').replace(/\-/,'').replace(' ', '').length == 10 : false);
 let trueCreatedAt = Value.query('data.created_at').as(createdAt => createdAt ? /((((19|20)([2468][048]|[13579][26]|0[48])|2000)-02-29|((19|20)[0-9]{2}-(0[4678]|1[02])-(0[1-9]|[12][0-9]|30)|(19|20)[0-9]{2}-(0[1359]|11)-(0[1-9]|[12][0-9]|3[01])|(19|20)[0-9]{2}-02-(0[1-9]|1[0-9]|2[0-8])))\s([01][0-9]|2[0-3]):([012345][0-9]):([012345][0-9]))/.test(createdAt) : false);
 
-module.exports = new Node({
+const node = new Node({
     className:'user.edit',
     template:resource('./templates/user-edit.tmpl'),
     binding:{
@@ -24,14 +24,14 @@ module.exports = new Node({
         truePhone:truePhone,
         trueCreatedAt:trueCreatedAt,
         notConfirmed:Value.query('data.confirmed').as(confirmed => !confirmed),
-        notSave:node => new Expression(
-            trueEmail,
-            truePhone,
-            trueCreatedAt,
-            (email, phone, created_at) => {
-                console.log(email, phone, created_at)
-                return false
-            }),
+        // notSave:node => new Expression(
+        //     Value.query(trueEmail),
+        //     Value.query(truePhone),
+            // trueCreatedAt,
+            // (email, phone, created_at) => {
+            //     console.log(email, phone, created_at)
+            //     return false
+            // }),
     },
     action:{
         cancel:function () {
@@ -60,3 +60,8 @@ module.exports = new Node({
         }
     }
 });
+
+
+let s = Value.query(node, 'data.email').as(email => validation.isEmail(email)).factory;
+console.log(s)
+module.exports = node
