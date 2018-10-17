2f336b607d1353dcb77e168961931bd286965879
agoalofalife <agoalofalife@gmail.com>
2018-10-17 15:31:54 +0300

    attempts to integrate the authentication

--- src/app/pages/init/index.js
+++ src/app/pages/init/index.js
@@ -1,18 +1,44 @@
 const Node = require('basis.ui').Node;
 const router = basis.require('basis.router');
+const DataObject = require('basis.data').Object;
+const service = require('app.service');
 
+var profile = new DataObject({
+    login: service.createAction({
+        needSignature: false,            // DEPRECATED иначе запрос не будет выполнятся
+        method: 'POST',
+        url: '/auth/login',
+        request: function(login, pwd){
+            return {                       // POST /login
+                params: {                    //
+                    email: login,              // login=[login]&password=[pwd]
+                    password: pwd              //
+                }
+            }
+        },
+        success: function(data){
+            console.log(data, 'success')
+            // предположим, сервер отдал JSON
+            // { "status": "ok", "session": "..." }
+            // service.openSession(data.session);
 
+            // сохраняем ключ сессии в cookie
+            // cookies.set('sessionKey', data.session);
+        }
+    })
+});
 module.exports = new Node({
     template: resource('./template/init.tmpl'),
     action:{
         signIn:function (e) {
-            // router.navigate('dashboard')
+            e.die();
+            profile.login(this.data.email, this.data.password);
         },
         updateEmail : function (e) {
-            console.log(e.sender.value);
+            this.update({email:e.sender.value});
         },
         updatePassword:function (e) {
-            console.log(e.sender.value);
+            this.update({password:e.sender.value});
         }
     }
 });
--- src/app/service.js
+++ src/app/service.js
@@ -2,15 +2,15 @@ var Service = require('basis.net.service').Service;
 
 var defaultService = new Service({
   isSecure: true,
-  transportClass: basis.net.ajax.Transport.subclass({
-    init: function(){
-      this.url = '/api/' + this.controller;
-      basis.net.ajax.Transport.prototype.init.call(this);
-    },
+  transportClass: {
+      init: function(){
+          this.url = '/api/' + this.controller;
+          basis.net.ajax.Transport.prototype.init.call(this);
+      },
       requestHeaders: {   // добавляем заголовок по умолчанию
           Accept: 'application/json'
       }
-  })
+  }
 });
 
 module.exports = defaultService;
