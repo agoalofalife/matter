d3802818af45c3ae623a64d9534aa629006af75c
agoalofalife <agoalofalife@gmail.com>
2018-10-18 08:18:56 +0300

    add action in auth component

--- src/app/components/auth/index.js
+++ src/app/components/auth/index.js
@@ -1,6 +1,18 @@
 const Node = require('basis.ui').Node;
-
+const auth = require('app.auth');
 
 module.exports = new Node({
     template: resource('./templates/auth.tmpl'),
+    action:{
+        signIn:function (e) {
+            e.die();
+            auth.login(this.data.email, this.data.password);
+        },
+        updateEmail : function (e) {
+            this.update({email:e.sender.value});
+        },
+        updatePassword:function (e) {
+            this.update({password:e.sender.value});
+        }
+    }
 });
